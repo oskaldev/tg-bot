c50d9fd10f7944343f0365e42bd9bbd879edba4d
oskal314 <artem-osipovartem3002@mail.ru>
2023-03-28 18:56:28 +0300

    initial game

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const TgApi = require('node-telegram-bot-api');
 const token = '';
 const bot = new TgApi(token, { polling: true });
+const chats = {};
+const { gameOptions, againOptions } = require('./options');
+
+
+const startGame = async (chatId) => {
+  await bot.sendMessage(chatId, 'Угадай цифру от 0 до 9 которую я загадал');
+  const randomNumber = Math.floor(Math.random() * 10);
+  chats[ chatId ] = randomNumber;
+  await bot.sendMessage(chatId, 'Отгадай', gameOptions);
+}
 
 const start = () => {
 
   bot.setMyCommands([
     { command: '/start', description: 'Приветствие' },
     { command: '/info', description: 'Проверить свой id' },
-    { command: '/delete_chat', description: 'Удалить чат' }
+    { command: '/game', description: 'Игра угадай цифру' }
   ])
   bot.on('message', async msg => {
     const text = msg.text;
@@ -20,7 +30,23 @@ const start = () => {
     if (text === '/info') {
       return bot.sendMessage(chatId, `Ваш id = ${msg.chat.id}`);
     }
+    if (text === '/game') {
+      return startGame(chatId);
+    }
     return bot.sendMessage(chatId, 'Выберите команду');
   });
+
+  bot.on('callback_query', async msg => {
+    const data = msg.data;
+    const chatId = msg.message.chat.id;
+    if (data === '/again') {
+      return startGame(chatId);
+    }
+    if (data == chats[ chatId ]) {
+      return bot.sendMessage(chatId, `Поздравляю ты отгадал цифру ${chats[ chatId ]}`, againOptions)
+    } else {
+      return bot.sendMessage(chatId, `К сожалению ты не отгадал это цифра ${chats[ chatId ]}`, againOptions)
+    }
+  })
 }
 start();
